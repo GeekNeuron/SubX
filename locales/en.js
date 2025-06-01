eaa4ee32adcf8acc63c89b2a664d4772a9d3869e
Geek Neuron <geekneuron@gmail.com>
2025-06-01 15:23:03 +0330

    Create en.js

---
+++ locales/en.js
@@ -0,0 +1,8 @@
+// src/locales/en.js
+// This file is effectively unused in the current "English-only" setup.
+// Texts are hardcoded in components.
+// Kept for potential future re-introduction of i18n.
+export const enTranslations = {
+  // This object would contain all translations if i18n was active.
+  // For now, components will use direct English strings.
+};
