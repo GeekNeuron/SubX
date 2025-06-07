d7a159ee20bfe1bde9f937e8c42e785ba25d08cb
Geek Neuron <geekneuron@gmail.com>
2025-06-07 22:18:50 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainApp from './App';
 
-// This is the entry point of the React application.
-// It gets the root DOM element and renders the MainApp component into it.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
