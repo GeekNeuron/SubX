6403bbc604c51f867ca9c7f862f17721f8615cfa
Geek Neuron <geekneuron@gmail.com>
2025-06-07 15:10:01 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainApp from './App'; // Assuming App.js exports MainApp
+import MainApp from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
